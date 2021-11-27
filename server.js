606dc57daefbc98803e51384a747d8f3edf55e12
Recep Öztürk <recep.fed@gmail.com>
2021-11-28 02:01:06 +0300

    solving heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv = require('dotenv')
-dotenv.config()
 
 const app = express();
 app.use(cors());
